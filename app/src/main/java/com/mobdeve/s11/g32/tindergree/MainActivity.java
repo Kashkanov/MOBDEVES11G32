782e724f025d908213bde30fb49b8d9e22289ef0
Chadi <chadi_turk@dlsu.edu.ph>
2021-07-30 16:13:36 +0800

    Created login layout xml file

--- app/src/main/java/com/mobdeve/s11/g32/tindergree/MainActivity.java
+++ app/src/main/java/com/mobdeve/s11/g32/tindergree/MainActivity.java
@@ -1,14 +1,26 @@
 package com.mobdeve.s11.g32.tindergree;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //temporarily set to activity_login just to test.
+        //A new activity should be created for the activity_login.
+        setContentView(R.layout.activity_login);
+        hideBar();
+    }
+
+    private void hideBar(){
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
