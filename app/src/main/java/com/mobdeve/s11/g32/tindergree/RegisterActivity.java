3a277073321af2a0a8f7bf7594a042934e7ecb51
Chadi <chadi_turk@dlsu.edu.ph>
2021-08-13 14:34:08 +0800

    created login + register activity

---
+++ app/src/main/java/com/mobdeve/s11/g32/tindergree/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s11.g32.tindergree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
