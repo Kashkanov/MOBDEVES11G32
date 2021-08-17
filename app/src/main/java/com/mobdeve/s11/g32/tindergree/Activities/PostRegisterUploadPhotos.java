57e1c3f3d26b74495b86edf7e9b190210f98fa89
GabbyNEW <gabriel_marquez@dlsu.edu.ph>
2021-08-17 13:12:52 +0800

    Add TODO comment for implicit intent

--- app/src/main/java/com/mobdeve/s11/g32/tindergree/Activities/PostRegisterUploadPhotos.java
+++ app/src/main/java/com/mobdeve/s11/g32/tindergree/Activities/PostRegisterUploadPhotos.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.mobdeve.s11.g32.tindergree.R;
 
 public class PostRegisterUploadPhotos extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_register_upload_photos);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // TODO: Implement Gallery pick implicit intent. I need the URI of the image.
     }
 }
