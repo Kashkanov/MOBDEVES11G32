23702cef941b913a2bbefd68ad62b37a5d3969ad
Chadi <chadi_turk@dlsu.edu.ph>
2021-08-20 08:40:05 +0800

    Created files for developer notes activity

---
+++ app/src/main/java/com/mobdeve/s11/g32/tindergree/Activities/DeveloperNotesActivity.java
@@ -0,0 +1,16 @@
+package com.mobdeve.s11.g32.tindergree.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mobdeve.s11.g32.tindergree.R;
+
+public class DeveloperNotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer_notes);
+    }
+}
