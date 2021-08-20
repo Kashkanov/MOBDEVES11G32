fb48f9c13a6043b4f4e927d5acc0693035a4a403
Chadi <chadi_turk@dlsu.edu.ph>
2021-08-20 22:14:53 +0800

    Implemented empty fields checker for PostRegisterUserDetails

--- app/src/main/java/com/mobdeve/s11/g32/tindergree/Activities/PostRegisterUserDetails.java
+++ app/src/main/java/com/mobdeve/s11/g32/tindergree/Activities/PostRegisterUserDetails.java
@@ -1,6 +1,4 @@
 package com.mobdeve.s11.g32.tindergree.Activities;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
@@ -17,44 +15,22 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
-import com.google.firebase.storage.FirebaseStorage;
 import com.mobdeve.s11.g32.tindergree.R;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class PostRegisterUserDetails extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    private FirebaseFirestore firestore;
-
     private ConstraintLayout clDogOption,clCatOption;
     private ImageView ivDogCheck,ivCatCheck;
-    private TextView tvDogOption,tvCatOption;
+    private TextView tvDogOption,tvCatOption,tvErrorMessage;
     private boolean ivDogIsChecked,ivCatIsChecked;
     private Button btnPostRegisterDetails;
-    private EditText etBirthday, etBreed, etName;
+    private EditText etBirthday,etBreed,etName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_register_user_details);
-
-        mAuth = FirebaseAuth.getInstance();
-        firestore = FirebaseFirestore.getInstance();
-
-        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-                .setPersistenceEnabled(false)
-                .build();
-        firestore.setFirestoreSettings(settings);
-
         initData();
 
 
@@ -67,10 +43,11 @@ public class PostRegisterUserDetails extends AppCompatActivity {
         ivCatCheck = findViewById(R.id.iv_post_register_user_details_check_cat);
         tvDogOption = findViewById(R.id.tv_post_register_user_details_dog);
         tvCatOption = findViewById(R.id.tv_post_register_user_details_cat);
+        tvErrorMessage = findViewById(R.id.tv_post_register_user_details_pet_error_message);
+        btnPostRegisterDetails = findViewById(R.id.btn_post_register_user_details_continue);
         etBirthday = findViewById(R.id.et_post_register_user_details_birthday);
         etBreed = findViewById(R.id.et_post_register_user_details_breed);
         etName = findViewById(R.id.et_post_register_user_details_name);
-        btnPostRegisterDetails = findViewById(R.id.btn_post_register_user_details_continue);
 
 
         ivDogIsChecked = false;
@@ -88,6 +65,7 @@ public class PostRegisterUserDetails extends AppCompatActivity {
                     tvDogOption.setTextColor(Color.parseColor("#FF914D"));
                     ivDogCheck.setVisibility(v.VISIBLE);
                     ivDogIsChecked = true;
+                    tvErrorMessage.setVisibility(View.GONE);
 
                     tvCatOption.setTextColor(Color.parseColor("#FF000000"));
                     ivCatCheck.setVisibility(v.GONE);
@@ -104,6 +82,7 @@ public class PostRegisterUserDetails extends AppCompatActivity {
 
                     ivCatCheck.setVisibility(v.VISIBLE);
                     ivCatIsChecked = true;
+                    tvErrorMessage.setVisibility(View.GONE);
 
                     tvDogOption.setTextColor(Color.parseColor("#FF000000"));
                     ivDogCheck.setVisibility(v.GONE);
@@ -115,8 +94,13 @@ public class PostRegisterUserDetails extends AppCompatActivity {
         btnPostRegisterDetails.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: Call the Firestore save data function here
-                saveDataToFirestore();
+
+                if(checkEmptyFields())
+                    return;
+
+                Intent i = new Intent(PostRegisterUserDetails.this,SwipeActivity.class);
+                startActivity(i);
+                finish();
             }
         });
     }
@@ -132,35 +116,34 @@ public class PostRegisterUserDetails extends AppCompatActivity {
         overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
     }
 
-    private void saveDataToFirestore() {
-        String uid = mAuth.getCurrentUser().getUid();
+    private boolean checkEmptyFields(){
+        boolean hasEmpty = false;
 
-        Map<String, Object> userData = new HashMap<>();
-        if (ivDogIsChecked == true)
-            userData.put("animal", "dog");
-        else if (ivCatIsChecked == true)
-            userData.put("animal", "cat");
-        userData.put("birthday", etBirthday.getText().toString());
-        userData.put("breed", etBreed.getText().toString());
-        userData.put("petName", etName.getText().toString());
-        userData.put("uid", uid);
+        String birthday = etBirthday.getText().toString();
+        String breed = etBreed.getText().toString();
+        String name = etName.getText().toString();
 
-        firestore.collection("Pets").document(uid).set(userData)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(SwipeActivity.firebaseLogKey, "DocumentSnapshot successfully written!");
+        if((ivDogIsChecked || ivCatIsChecked) == false){
+            tvErrorMessage.setVisibility(View.VISIBLE);
+            tvErrorMessage.requestFocus();
+            hasEmpty = true;
+        }
+        else if(birthday.isEmpty()){
+            etBirthday.setError("Please enter your pets birthday!");
+            etBirthday.requestFocus();
+            hasEmpty = true;
+        }
+        else if(breed.isEmpty()){
+            etBreed.setError("Please enter your pets breed!");
+            etBreed.requestFocus();
+            hasEmpty = true;
+        }
+        else if(name.isEmpty()){
+            etName.setError("Please enter your pets name!");
+            etName.requestFocus();
+            hasEmpty = true;
+        }
 
-                        Intent i = new Intent(PostRegisterUserDetails.this,SwipeActivity.class);
-                        startActivity(i);
-                        finish();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(SwipeActivity.firebaseLogKey, "Error writing document", e);
-                    }
-                });
+        return hasEmpty;
     }
 }
