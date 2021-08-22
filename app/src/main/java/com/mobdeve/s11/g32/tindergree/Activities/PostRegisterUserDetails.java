9f2d35cbeb972928a0bbcee7d30f9fd27b2d92c7
Chadi <chadi_turk@dlsu.edu.ph>
2021-08-22 15:30:36 +0800

    Added date picker for PostRegisterUserDetails and disabled user input through text

--- app/src/main/java/com/mobdeve/s11/g32/tindergree/Activities/PostRegisterUserDetails.java
+++ app/src/main/java/com/mobdeve/s11/g32/tindergree/Activities/PostRegisterUserDetails.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.ActivityOptions;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -13,6 +15,7 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -27,6 +30,7 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.storage.FirebaseStorage;
 import com.mobdeve.s11.g32.tindergree.R;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,16 +38,19 @@ public class PostRegisterUserDetails extends AppCompatActivity {
 
     private ConstraintLayout clDogOption,clCatOption;
     private ImageView ivDogCheck,ivCatCheck;
-    private TextView tvDogOption,tvCatOption,tvErrorMessage;
+    private TextView tvDogOption,tvCatOption,tvErrorMessage,tvBirthday;
     private boolean ivDogIsChecked,ivCatIsChecked;
     private Button btnPostRegisterDetails;
-    private EditText etBirthday,etBreed,etName,etAboutPet;
+    private EditText etBreed,etName,etAboutPet;
+
+    private DatePickerDialog datePickerDialog;
+    ConstraintLayout clBirthdayContainer;
 
     private FirebaseAuth mAuth;
     private FirebaseStorage storage;
     private FirebaseFirestore firestore;
 
-    //TODO For the date, there should be a date picker instead of the user inputting text.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,7 +85,15 @@ public class PostRegisterUserDetails extends AppCompatActivity {
         tvCatOption = findViewById(R.id.tv_post_register_user_details_cat);
         tvErrorMessage = findViewById(R.id.tv_post_register_user_details_pet_error_message);
         btnPostRegisterDetails = findViewById(R.id.btn_post_register_user_details_continue);
-        etBirthday = findViewById(R.id.et_post_register_user_details_birthday);
+
+        initDatePicker();
+
+        tvBirthday = findViewById(R.id.tv_post_register_user_details_birthday_value);
+        clBirthdayContainer = findViewById(R.id.cl_post_register_user_details_birthday);
+
+
+        tvBirthday.setText(getTodaysDate());
+
         etBreed = findViewById(R.id.et_post_register_user_details_breed);
         etName = findViewById(R.id.et_post_register_user_details_name);
         etAboutPet = findViewById(R.id.et_post_register_user_details_about_pet);
@@ -133,6 +148,13 @@ public class PostRegisterUserDetails extends AppCompatActivity {
                 saveDataToFirestore();
             }
         });
+
+        clBirthdayContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatePicker(v);
+            }
+        });
     }
 
     @Override
@@ -154,7 +176,7 @@ public class PostRegisterUserDetails extends AppCompatActivity {
             userData.put("animal", "dog");
         else if (ivCatIsChecked == true)
             userData.put("animal", "cat");
-        userData.put("birthday", etBirthday.getText().toString());
+        userData.put("birthday", tvBirthday.getText().toString());
         userData.put("breed", etBreed.getText().toString());
         userData.put("petName", etName.getText().toString());
         userData.put("uid", uid);
@@ -181,7 +203,7 @@ public class PostRegisterUserDetails extends AppCompatActivity {
     private boolean checkEmptyFields(){
         boolean hasEmpty = false;
 
-        String birthday = etBirthday.getText().toString();
+        String birthday = tvBirthday.getText().toString();
         String breed = etBreed.getText().toString();
         String name = etName.getText().toString();
         String aboutPet = etAboutPet.getText().toString();
@@ -192,8 +214,8 @@ public class PostRegisterUserDetails extends AppCompatActivity {
             hasEmpty = true;
         }
         else if(birthday.isEmpty()){
-            etBirthday.setError("Please enter your pets birthday!");
-            etBirthday.requestFocus();
+            tvBirthday.setError("Please enter your pets birthday!");
+            tvBirthday.requestFocus();
             hasEmpty = true;
         }
         else if(breed.isEmpty()){
@@ -214,4 +236,90 @@ public class PostRegisterUserDetails extends AppCompatActivity {
 
         return hasEmpty;
     }
+
+    private void initDatePicker(){
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month = month + 1;
+                String date = makeDateString(day,month,year);
+                tvBirthday.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        datePickerDialog = new DatePickerDialog(this,dateSetListener,year,month,day);
+
+    }
+
+    public String makeDateString(int day, int month, int year){
+
+        return getMonthFormat(month) + " " + day + " " + year;
+
+    }
+
+    private String getMonthFormat(int month){
+
+        String monthReturned = "";
+
+        switch(month)
+        {
+            case 1:
+                monthReturned = "January";
+                break;
+            case 2:
+                monthReturned =  "February";
+                break;
+            case 3:
+                monthReturned =  "March";
+                break;
+            case 4:
+                monthReturned =  "April";
+                break;
+            case 5:
+                monthReturned =  "May";
+                break;
+            case 6:
+                monthReturned =  "June";
+                break;
+            case 7:
+                monthReturned = "July";
+                break;
+            case 8:
+                monthReturned =  "August";
+                break;
+            case 9:
+                monthReturned =  "September";
+                break;
+            case 10:
+                monthReturned =  "October";
+                break;
+            case 11:
+                monthReturned =  "November";
+                break;
+            case 12:
+                monthReturned =  "December";
+                break;
+        }
+            return monthReturned;
+    }
+
+    public void openDatePicker(View view){
+        datePickerDialog.show();
+    }
+
+    private String getTodaysDate(){
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month +1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        return makeDateString(day,month,year);
+
+    }
+
 }
